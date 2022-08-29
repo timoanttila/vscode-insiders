2b8d41ec12d30ec4ae71eddb7ae65bd27fd2ae9a
Timo Anttila <timo@tuspe.com>
2022-08-30 00:58:19 +0300

    Pahaporo theme

--- extensions/vangware.dark-plus-material-3.1.0/src/RGBA.ts
+++
@@ -1,9 +0,0 @@
-/**
- * RGBA Quadruple.
- */
-export type RGBA = readonly [
-	red: number,
-	green: number,
-	blue: number,
-	alpha: number,
-];
--- extensions/vangware.dark-plus-material-3.1.0/src/rgbaToHex.ts
+++
@@ -1,7 +0,0 @@
-import type { RGBA } from "./RGBA.js";
-
-/**
- * Takes an RGBA quadruple and returns a hex color.
- */
-export const rgbaToHex = (rgba: RGBA) =>
-	`#${rgba.map(value => value.toString(16).padStart(2, "0")).join("")}`;
